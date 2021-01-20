89c8aab1e5d8d944dbf25939a7f85aeb6708c93a
David <davidlinteau27@gmail.com>
2021-01-20 15:15:56 -0500

    Create space_to_underscore.py

---
+++ scripts/space_to_underscore.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jan 13 12:33:11 2021
+
+@author: David
+"""
+
+import glob, os
+
+# for filename in glob.glob('../first_week_raw_data/data/Angles/55/00 Other Sources/Ba-133 000.Chn'):
+#     new_name = filename.replace(' ', '_')
+#     os.rename(filename, new_name)
+    
+def replace(parent):
+    for path, folders, files in os.walk(parent):
+        for f in files:
+            os.rename(os.path.join(path, f), os.path.join(path, f.replace(' ', '_')))
+        for i in range(len(folders)):
+            new_name = folders[i].replace(' ', '_')
+            os.rename(os.path.join(path, folders[i]), os.path.join(path, new_name))
+            folders[i] = new_name
+            
+replace('../first_week_raw_data')
