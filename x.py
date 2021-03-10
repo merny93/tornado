133da113611c19f1c33c6ed99981f01559737ad2
Simon Tartakovksy <simtart@gmail.com>
2021-03-10 14:53:51 -0500

    push this shit

--- x.py
+++ x.py
@@ -19,7 +19,7 @@ def double_fit(x, *argvs):
     #now what if we tried something spicy ;)
     #double 
     # args is x then [mean1, sigma1, a1, gamma1, mean2, sigma2, a2, gamma2, *poly]
-    return sum([argvs[2+i]* voigt_profile(x - argvs[0+i], argvs[1+i], argvs[3+i], out=None) for i in [0,4]]) + np.polyval(argvs[8:], x) 
+    return sum([argvs[2+i]* voigt_profile(x - argvs[0+i], argvs[1+i], argvs[3+i], out=None) for i in [0]]) + np.polyval(argvs[4:], x) 
     #that line sums over the two voigt profiles and then adds a polynomial as discribed by mit
 
 
@@ -55,10 +55,32 @@ for window in windows:
     #optimize***************
     #first get a easy run-through using the gaussian
     popt,pcov = curve_fit(fit_func, x,y, p0=[sum(window)/2,1, 250, 0.7], sigma=noise, absolute_sigma=True)
+    fig = plt.figure()
+    gs = fig.add_gridspec(2, hspace = 0, height_ratios = [3,1])
+    axs = gs.subplots(sharex= True)
 
+    axs[0].scatter(x,y)
+    axs[0].plot(x,fit_func(x,*popt))
+    axs[1].scatter(x, y- fit_func(x, *popt))
+    plt.show()
     #this gives a really good first guess for the next step
-    popt,pcov = curve_fit(double_fit, x,y, p0=[popt[0] - 0.5,popt[1], popt[2]/2, 0,popt[0] + 0.5,popt[1], popt[2]/2, 0,0,0], 
-                        sigma=noise, absolute_sigma=True, maxfev=1000000, bounds=([0, 0,0,0,popt[0],0,0,0, -np.inf, -np.inf],[popt[0], np.inf,np.inf,np.inf,np.inf,np.inf,np.inf,np.inf, np.inf, np.inf]))
+    p0 = [popt[0], 0.5 *popt[1], 2*popt[2], 0.1, 
+          0,0]
+    
+    ##IF U WANT TO PLOT THE FIRST GEUSS
+    # plt.scatter(x,y)
+    # plt.plot(x,double_fit(x,*p0))
+    # plt.show()
+    # bounds = ([0, 0,0,0,
+    #            popt[0],0,0,0, 
+    #            0,0,0,0,
+    #            -np.inf, -np.inf],
+    #            [popt[0], np.inf,np.inf,np.inf,
+    #            np.inf,np.inf,np.inf,np.inf, 
+    #            popt[0], np.inf,np.inf,np.inf,
+    #            np.inf,np.inf])
+    popt,pcov = curve_fit(double_fit, x,y, p0=p0, sigma=noise, absolute_sigma=True, maxfev=1000000) #, bounds=bounds)
+    print(popt)
     #now get a prediction for residual calculations!
     y_pred = double_fit(x,*popt)
     
@@ -70,8 +92,9 @@ for window in windows:
     print("position is,", popt[0], "+/-", np.sqrt(np.diag(pcov))[0] )
     
     #save to the results
+    #order is critical here!
     theta_fit.append((popt[0], np.sqrt(np.diag(pcov))[0]))
-    theta_fit.append((popt[4], np.sqrt(np.diag(pcov))[4]))
+   
 
     #for plot get high res
     x_high = np.linspace(x[0], x[-1], num=250)
@@ -87,7 +110,8 @@ for window in windows:
     #fig.suptitle("Sample fits")
 
     axs[0].plot(x_high,y_high, label="fit", color="magenta")
-    axs[0].scatter(x, y, label="data", marker="x", s=25, c="black" , linewidth= 2)
+    axs[0].scatter(x, y, label="data", marker="x", s=25, c="black", linewidth=2)
+    axs[0].errorbar(x, y, yerr = noise, linestyle="")
     
     axs[0].legend()
     axs[1].scatter(x, y_pred-y, marker="x", s=25, c="black" , linewidth= 2)
@@ -121,8 +145,8 @@ for window in windows:
 def shape_func(lmda_sqd_times_combined_hkl, a):
     return np.rad2deg(np.arcsin(np.sqrt(lmda_sqd_times_combined_hkl/ (4* a**2)))) * 2
 
-hkls = [3,4,8,11, 12]
-lmdas = [0.15443e-9,0.15406e-9][::-1]
+hkls = [3,4,8,11,12]
+lmdas = sorted([0.1540562e-9])
 
 from itertools import product
 x_s = np.array(list(map(lambda x: x[0] * x[1]**2, product(hkls, lmdas))))
@@ -132,6 +156,45 @@ popt,pcov = curve_fit(shape_func,x_s, y_s, p0= [3.5e-10], sigma=n_s)
 print(n_s)
 print(popt[0], "+/-", np.sqrt(np.diag(pcov))[0])
 print(np.sum((y_s - shape_func(x_s, *popt)/n_s)**2))
-plt.scatter(x_s, y_s)
-plt.plot(x_s, shape_func(x_s, *popt))
+
+x_high = np.linspace(x_s[0], x_s[-1], num=250)
+y_high = shape_func(x_high, *popt)
+y_pred = shape_func(x_s, *popt)
+
+
+#get spify
+plt.rcParams.update({'font.size': 16})
+
+
+fig = plt.figure()
+gs = fig.add_gridspec(2, hspace = 0, height_ratios = [3,1])
+axs = gs.subplots(sharex= True)
+#fig.suptitle("Sample fits")
+
+axs[0].plot(x_high,y_high, label="fit", color="magenta")
+axs[0].scatter(x_s, y_s, label="data", marker="x", s=25, c="black" , linewidth= 2)
+
+axs[0].legend()
+axs[1].scatter(x_s, y_pred-y_s, marker="x", s=25, c="black" , linewidth= 2)
+axs[1].axhline(y=0,c="magenta", linestyle="--")
+axs[1].errorbar(x_s,  y_pred-y_s, yerr = n_s, linestyle="")
+axs[1].set_ylim(-10,10)
+
+#get rid of overlap
+to_kills = axs[0].yaxis.get_ticklabels()
+to_kills[0].set_visible(False)
+
+#reduce density of residual plot
+for n, label in enumerate(axs[1].yaxis.get_ticklabels()):
+    if n % 2 != 0:
+        label.set_visible(False)
+
+#add the y axis label
+axs[0].set(ylabel=r"$\theta$", fontsize = 15)
+axs[1].set(ylabel="Residuals")
+#do something im not sure what
+for ax in axs:
+    ax.label_outer()
+    ax.set(xlabel=r"$\lambda^2(h^2+k^2+l^2)$", fontsize = 15)
+
 plt.show()
