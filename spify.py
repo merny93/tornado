2b6ec9986cab4f565458f53bb1aef002536dd5b8
Simon Tartakovksy <simtart@gmail.com>
2021-03-15 14:29:52 -0400

    pushhhh

--- spify.py
+++ spify.py
@@ -2,7 +2,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 
-def residual_plot(x,data,noise,func,params, xlabel, ylabel1,ylabel2, filename):
+def residual_plot(x,data,noise,func,params, xlabel, ylabel1,ylabel2, filename, renorm = False):
     #get spify
     plt.clf()
     plt.rcParams.update({'font.size': 24})
@@ -14,21 +14,26 @@ def residual_plot(x,data,noise,func,params, xlabel, ylabel1,ylabel2, filename):
     #fig.suptitle("Sample fits")
 
     #generate the high
+    
     x_high = np.linspace(x[0],x[-1], num=250)
     y_high = func(x_high, *params)
     y_pred = func(x,*params)
+
+    if renorm:
+        x_high = x_high/1e-19
+        x = x/1e-19
 
     axs[0].plot(x_high,y_high, label="fit", color="magenta")
     axs[0].scatter(x, data, label="data", marker=".", s=25, c="black", linewidth=2)
     axs[0].errorbar(x, data, yerr = noise, linestyle="", c="black")
     
-    axs[0].legend(fontsize = 18)
+    axs[0].legend(loc = 'upper left', fontsize = 18)
     axs[1].scatter(x, y_pred-data, marker=".", s=25, c="black" , linewidth= 2)
     axs[1].errorbar(x, y_pred-data, yerr = noise, linestyle="", c="black")
     axs[1].axhline(y=0,c="magenta", linestyle="--")
-    axs[1].set_xlim(x[0], x[-1])
     # axs[1].set_yticklabels(labels = [-10,0,10],fontsize = 5)
-    axs[1].set_yticks([-10,0,10])
+    # axs[1].set_yticks([-10,0,10])
+
 
     
     #get rid of overlap
@@ -41,15 +46,15 @@ def residual_plot(x,data,noise,func,params, xlabel, ylabel1,ylabel2, filename):
             label.set_visible(False)
 
     #add the y axis label
-    axs[0].set(ylabel=ylabel1)
-    axs[1].set(ylabel=ylabel2)
+    axs[0].set(ylabel="{}".format(ylabel1))
+    axs[1].set(ylabel="{}".format(ylabel2))
     #do something im not sure what
     for ax in axs:
         ax.label_outer()
-        ax.set(xlabel=r"{}".format(xlabel))
+        ax.set(xlabel="{}".format(xlabel))
 
     plt.tight_layout()
-    plt.savefig('./figures/{}_voigt.png'.format(filename))
+    plt.savefig('./figures/{}.png'.format(filename))
 
 
 def lattice_alloy_plot(data, litterature, header):
--- x.py
+++ x.py
@@ -4,6 +4,7 @@ import matplotlib.pyplot as plt
 from scipy.optimize import curve_fit
 from scipy.special import voigt_profile
 import spify
+import os
 #load pure copper data!
 
 #the new fitting fit needs to work with different h,k,l
@@ -15,7 +16,7 @@ def shape_func(lmda_sqd_times_combined_hkl, a, theta_0):
 def fit_func(x, *argvs):
     #start with gaussian
     #args is x then [mean, sigma,  a, c]
-    return np.array(argvs[2] * np.exp(-((x-argvs[0])/argvs[1])**2) + argvs[3], dtype=float)
+    return argvs[2] * np.exp(-((x-argvs[0])/argvs[1])**2) + argvs[3]
 
 ##DOUGBLE PEAK MIT FIT
 def double_fit(x, *argvs):
@@ -24,6 +25,9 @@ def double_fit(x, *argvs):
     # args is x then [mean1, sigma1, a1, gamma1, mean2, sigma2, a2, gamma2, *poly]
     return sum([argvs[2+i]* voigt_profile(x - argvs[0+i], argvs[1+i], argvs[3+i], out=None) for i in [0]]) + np.polyval(argvs[4:], x) 
     #that line sums over the two voigt profiles and then adds a polynomial as discribed by mit
+
+def line(x, a,b):
+    return a*x+b
 
 
 
@@ -117,16 +121,11 @@ def fitter(filename, windows):
         #save to the results
         #order is critical here!
         theta_fit.append((popt[0], np.sqrt(np.diag(pcov))[0]))
-    
-
-        #for plot get high res
-        x_high = np.linspace(x[0], x[-1], num=250)
-        y_high = double_fit(x_high, *popt)
 
         #get spify
         spify.residual_plot(x,y,noise, double_fit, popt,
                             r"$2\theta \ (^\circ)$","Counts","Residuals", 
-                            filename+"_"+str(i+1))
+                            filename+"_"+str(i+1)+"_voigt")
         
         peaks_data['peak_{}'.format(i)] = {}
         peaks_data['peak_{}'.format(i)]['popt'] = popt[0]
@@ -155,78 +154,60 @@ def fitter(filename, windows):
     y_pred = shape_func(x_s, *popt)
 
 
-    #get spify
-    plt.clf()
-    plt.rcParams.update({'font.size': 24})
-
-
-    fig = plt.figure()
-    gs = fig.add_gridspec(2, hspace = 0, height_ratios = [3,1])
-    axs = gs.subplots(sharex= True)
-    plt.rcParams.update({'font.size': 24})
-    #fig.suptitle("Sample fits")
-
-    axs[0].plot(x_high/1e-19,y_high, label="fit", color="magenta")
-    axs[0].scatter(x_s/1e-19, y_s, label="data", marker=".", s=25, c="black" , linewidth= 2)
-    axs[0].legend(loc = 'upper left', fontsize = 18)
-
-    axs[1].scatter(x_s/1e-19, y_pred-y_s, marker=".", s=25, c="black" , linewidth= 2)
-    axs[1].axhline(y=0,c="magenta", linestyle="--")
-    axs[1].errorbar(x_s/1e-19,  y_pred-y_s, yerr = n_s, linestyle="", color='black')
-    axs[1].set_yticks([-0.02,0, 0.02])
-    # axs[1].set_ylim(-10,10)
-
-    #get rid of overlap
-    to_kills = axs[0].yaxis.get_ticklabels()
-    to_kills[0].set_visible(False)
-
-    #reduce density of residual plot
-    for n, label in enumerate(axs[1].yaxis.get_ticklabels()):
-        if n % 2 != 0:
-            label.set_visible(False)
-
-    #add the y axis label
-    axs[0].set_ylabel(r"$2\theta \ (^{\circ} )$")
-    axs[1].set_ylabel("Residuals") 
-    #do something im not sure what
-    for ax in axs:
-        ax.label_outer()
-        ax.set_xlabel(r"$\lambda^2(h^2+k^2+l^2) \ (10^{-19} \ \mathrm{m}^2)$")
-
-    plt.tight_layout()
-    plt.savefig('./figures/{}_lattice.png'.format(filename))
+    spify.residual_plot(x_s, y_s, n_s,shape_func, popt, 
+                        r"$\lambda^2(h^2+k^2+l^2) \ (10^{-19} \ \mathrm{m}^2)$",
+                         r"$2\theta \ (^{\circ} )$",
+                         "Residuals", 
+                         filename + "_lattice", renorm=True)
     peaks_data['line_popt'] = popt
     peaks_data['line_unc'] = np.sqrt(np.diag(pcov))
     peaks_data['line_chi'] = chi_sqd
     return peaks_data
 
-if __name__ == '__main__':
-    ##
-    filenames = ["Cu_03_09_20", "Cu75Ni25","Cu50Ni50","Cu25Ni75", "Ni_03_09_20"]
-    bins = [[[41.5,45.5],[48,53],[72,77],[88,93], [93,98]],
-            [[41.5,49],[48,56],[72,80],[88,95], [93,99]],
-            [[41.5,48],[48,56],[72,80],[88,95], [95,99]],
-            [[41.5,47],[48,56],[72,80],[89,96], [95,99]],
-            [[41.5,50],[48,56],[72,80],[90,96], [97,101]]]
+def full_anal(path_, bins, print=False):
+    filenames = os.listdir(path_)
+    filenames = list(map(lambda x: x.split(".")[0], filenames))
+    # filenames = ["Cu_03_09_20", "Cu75Ni25","Cu50Ni50","Cu25Ni75", "Ni_03_09_20"]
     total_data = {}
     for i in range(len(filenames)):
         total_data['{}'.format(filenames[i])] = (fitter(filenames[i], bins[i]))
     # print(total_data)
-    try:
-        import pprint 
-        pp = pprint.PrettyPrinter(indent=4)
-        pp.pprint(total_data)
-    except:
-        print(total_data)
-
-    plot_data = [[0,25,50,75,100],[],[],['Data']]
-    for name in filenames:
-        plot_data[1].append(total_data[name]["line_popt"][0])
-        plot_data[2].append(total_data[name]["line_unc"][0])
+    if print:
+        try:
+            import pprint 
+            pp = pprint.PrettyPrinter(indent=4)
+            pp.pprint(total_data)
+        except:
+            print(total_data)
     
-    plot_litterature = [[0,100],
-                        [361.49e-12, 352.4e-12],
-                        ['Litterature']]
-    spify.lattice_alloy_plot(plot_data, plot_litterature, 'Cu-Ni')
+    
+    y_data = [[],[]]
+    for name in filenames:
+        y_data[0].append(total_data[name]["line_popt"][0])
+        y_data[1].append(total_data[name]["line_unc"][0])
+    
+    y_data[0] = np.array(y_data[0], dtype = float)
+    y_data[1] = np.array(y_data[1], dtype = float)
+    # plot_litterature = [[0,100],
+    #                     [361.49e-12, 352.4e-12],
+    #                     ['Litterature']]
+    # spify.lattice_alloy_plot(plot_data, plot_litterature, 'Cu-Ni')
+    x = np.array([25,50,75,0, 100] ,dtype = float)
+    popt,pcov = curve_fit(line,x,y_data[0], p0= [1,1], sigma=y_data[1])
+
+    chi_sqd = np.sum((y_data[0] -line(np.array(x), *popt))**2/y_data[1]**2)/len(y_data[0]-2)
+
+    spify.residual_plot(x, y_data[0], y_data[1],
+                        line, popt, "Lattice Parameter","Nickel Concentration","Residuals", 
+                        path_[-15:] + "_vegard")
+
+if __name__ == '__main__':
+    copper_bins = [[[41.5,49],[48,56],[72,80],[88,95], [93,99]],
+            [[41.5,48],[48,56],[72,80],[88,95], [95,99]],
+            [[41.5,47],[48,56],[72,80],[89,96], [95,99]],
+            [[41.5,45.5],[48,53],[72,77],[88,93], [93,98]],
+            [[41.5,50],[48,56],[72,80],[90,96], [97,101]]]
+    full_anal('./X-Ray/data/copper_nickel_series', copper_bins, print=True)
+    
 
 
